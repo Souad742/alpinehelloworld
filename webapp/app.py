67b9d8d44e2c7a64ac8e79e3e6c018b26305abdf
Souad742 <72011697+Souad742@users.noreply.github.com>
2025-11-14 09:47:14 +0100

    Update app.py
    Signed-off-by: Souad742 <72011697+Souad742@users.noreply.github.com>

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,4 @@
-import os        # import inutile
-import sys       # encore un import inutile
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,16 +8,8 @@ app = Flask(__name__)
 def h():  
     return 'Hello world!'  # pas de formatage, pas de f-string
 
-# Code smell: logique inutile dans le main
-if __name__ == '__main__':
-    x = 42
-    if x == 42:   # condition inutile
-        app.run(host='0.0.0.0')  # pas de port spécifié, pas de debug
 
 
 
-# Code smell: logique inutile dans le main
-if __name__ == '__main__':
-    x = 42
-    if x == 42:   # condition inutile
-        app.run(host='0.0.0.0')  # pas de port spécifié, pas de d
+
+
